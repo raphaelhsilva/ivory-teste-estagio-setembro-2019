43a7c954140fa3b1d1372e80decab10426544bc6
Raphael da Silva <47163339+raphaelhsilva@users.noreply.github.com>
2019-09-05 00:47:31 -0300

    Upload dos Arquivos
    Upload dos arquivos para o processo seletivo Ivory

---
+++ Questao1/Questao1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Questao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 5;
+            int resultado;
+
+            resultado = Calcular(numero);
+
+            Console.WriteLine("Resultado: {0}", resultado);
+            Console.ReadKey();
+        }
+
+        static int Calcular(int numero)
+        {
+            int resultado;
+            if (numero <= 1)
+            {
+                return (1);
+            }
+            else
+            {
+                /*Não é possivel, chamar uma função dentro da própria função dessa maneiro.
+                 Isso faz com que o programa não execute devidamento por um problema de loop infinito
+                */
+
+                //Solução
+                //resultado = numero * (numero -= 1);
+                resultado = numero * Calcular(numero--);
+                return (resultado);
+            }
+        }
+    }
+}
