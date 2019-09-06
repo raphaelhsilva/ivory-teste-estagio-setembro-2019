aba5e0a3fcec96867ca74eaa4b5a2fabed493019
Raphael da Silva <47163339+raphaelhsilva@users.noreply.github.com>
2019-09-06 08:25:02 -0300

    Melhorias na Questão 2
    Foi feito algumas melhorias na estrutura do código da questão 2.

---
+++ Questão2 (Melhoria)/MatrixNames/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace ProgramaDeEstagioIvory
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string name; //Armazena a entrada do usuário
+            string[,] m; //Matriz que armazena os dados para exibir
+            char[] splitName = new char[1]; //Armazena os caracteres separados da entrada do usuário
+
+            //Entrada do Usuário
+            Console.WriteLine("Escreva um nome: ");
+
+            name = Console.ReadLine();
+            splitName = name.ToCharArray();
+            m = new string[3, name.Length + 3];
+            int n1 = 0; //Contador
+
+            Console.WriteLine(); //Linha para organização da exibição
+
+            //Criação da implementação na Matriz
+
+            //Eixo X da Matriz (linhas)
+            for (int i = 0; i < 3; i++)
+            {
+                //Eixo Y da Matriz (Colunas)
+                for (int o = 0; o < name.Length + 3; o++)
+                {
+                    //Atribuindo Valores dentro dos campos da Matriz na primeira Linha
+                    if (i < 1 && o < name.Length + 2)
+                    {
+                        n1++;
+                        string n2 = Convert.ToString(n1);
+                        Console.Write(m[i, o] = n2 + "\t");
+                    }
+                    //Quebra de Linha
+                    else if (i < 1 && o == name.Length + 2)
+                    {
+                        Console.WriteLine();
+                    }
+
+                    //Atribuindo Valor dentro do campo da Segunda Linha Primeira Coluna
+                    if (i == 1 && o < 2)
+                    {
+                        int n3 = n1;
+                        n3 = (name.Length + 2) * 2 + 2;
+                        string n2 = Convert.ToString(n3);
+                        Console.Write(m[i, o] = n2 + "\t");
+                    }
+
+                    //Atribuindo Valores na Segunda linha da Matriz
+                    if (i == 1 && o <= name.Length - 1)
+                    {
+                        n1++;
+                        string n2 = Convert.ToString(n1);
+                        int a = -1;
+
+                        //Atribuindo o Nome escolhido na Matriz
+                        while (a < name.Length - 1)
+                        {
+                            a++;
+                            Console.Write(m[i, o] = splitName[a].ToString() + "\t");
+                        }
+
+                        //Atribuindo o Valor na Ultima coluna da Segunda linha
+                        if (i == 1)
+                        {
+                            n2 = Convert.ToString(n1);
+                            Console.Write(m[i, o] = n2);
+                        }
+
+                        //Atribundo Valores da Ultima linha da Matriz
+                        Console.WriteLine();
+                        int n3 = (name.Length + 2) * 2 + 2;
+
+                        for (int y = 0; y < name.Length + 2; y++)
+                        {
+                            n3--;
+                            n2 = Convert.ToString(n3);
+                            Console.Write(m[i, o] = n2 + "\t");
+                        }
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
